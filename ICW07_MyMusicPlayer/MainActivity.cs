432fe732ffcf0d5147c1355e2bd6b41841f3fd64
Guruspeaker <paul.christian001@mymdc.net>
2017-11-28 08:36:19 -0500

    Add project files.

---
+++ ICW07_MyMusicPlayer/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace ICW07_MyMusicPlayer
+{
+    [Activity(Label = "ICW07_MyMusicPlayer", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
